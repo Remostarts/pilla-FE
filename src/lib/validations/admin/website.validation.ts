81a617323d5781667154ee7e779652347bb85c0e
jahir sheikh <jahirsheikh211@gmail.com>
2024-10-16 17:32:46 +0530

    MODIFIED: personal dashboard home section and fix the file upload error in business dashboard.

--- src/lib/validations/admin/website.validation.ts
+++ src/lib/validations/admin/website.validation.ts
@@ -5,7 +5,7 @@ export const blogPostSchema = z.object({
   tag: z.string().min(1, 'Tag is required'),
   title: z.string().min(1, 'Title is required'),
   description: z.string().min(1, 'Description is required'),
-  image: z.any().optional(),
+  image: z.string().min(1, 'Image is required'),
 });
 export type TBlogPost = z.infer<typeof blogPostSchema>;
 
@@ -14,7 +14,7 @@ export const careerPostSchema = z.object({
   tag: z.string().min(1, 'Tag is required'),
   title: z.string().min(1, 'Title is required'),
   content: z.string().min(1, 'Content is required'),
-  image: z.any().optional(),
+  image: z.string().min(1, 'Image is required'),
   remote: z.string().min(1, 'Remote is required'),
   employmentType: z.string().min(1, 'Employment Type is required'),
 });
